159a97703fe2729655debde03abfe85c10a158b1
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-03-22 09:02:55 +0530

    working on service level logs

--- src/ReadKubeLogs.Service/Controllers/KubeController.cs
+++ src/ReadKubeLogs.Service/Controllers/KubeController.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using k8s;
+using k8s.Models;
+
 namespace ReadKubeLogs.Service.Controllers
 {
     [Route("api/[controller]")]
@@ -36,23 +38,12 @@ namespace ReadKubeLogs.Service.Controllers
             }
             return Json(content);
         }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
+        [HttpGet("{serviceName}/{namespaceName}")]
+        public  IActionResult GetServiceLog(string serviceName, string namespaceName)
         {
-        }
+            V1Service services = kubeClient.ReadNamespacedService(serviceName, namespaceName);
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Json(services);
         }
     }
 }
